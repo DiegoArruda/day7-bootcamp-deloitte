6fb9f7e4be74b66507747fe318ba018cc2bab85b
DiegoArruda <arrudadiego1@gmail.com>
2025-05-14 16:43:31 -0300

    missing id from client dto

--- day7-bootcamp/src/main/java/com/deloitte/day7_bootcamp/controller/dto/ClientDTO.java
+++ day7-bootcamp/src/main/java/com/deloitte/day7_bootcamp/controller/dto/ClientDTO.java
@@ -1,17 +1,24 @@
 package com.deloitte.day7_bootcamp.controller.dto;
 
 import com.deloitte.day7_bootcamp.domain.model.Client;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ClientDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
 
     public Client toEntity(){
         Client client = new Client();
+        client.setId(this.getId());
         client.setName(this.getName());
         client.setEmail(this.getEmail());
         return client;
@@ -19,6 +26,7 @@ public class ClientDTO {
 
     public static ClientDTO fromEntity(Client client){
         ClientDTO dto = new ClientDTO();
+        dto.setId(client.getId());
         dto.setName(client.getName());
         dto.setEmail(client.getEmail());
         return dto;
