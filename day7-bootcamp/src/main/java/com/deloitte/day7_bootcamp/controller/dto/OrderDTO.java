4a9bb9fecd5fee5b4e4155f062971cf32eebb2d7
DiegoArruda <arrudadiego1@gmail.com>
2025-05-14 16:42:14 -0300

    missing id from order and its dto

--- day7-bootcamp/src/main/java/com/deloitte/day7_bootcamp/controller/dto/OrderDTO.java
+++ day7-bootcamp/src/main/java/com/deloitte/day7_bootcamp/controller/dto/OrderDTO.java
@@ -3,6 +3,9 @@ package com.deloitte.day7_bootcamp.controller.dto;
 import com.deloitte.day7_bootcamp.domain.model.Client;
 import com.deloitte.day7_bootcamp.domain.model.Order;
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
@@ -12,6 +15,9 @@ import org.springframework.beans.factory.annotation.Value;
 @Data
 @NoArgsConstructor
 public class OrderDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Positive
     @Min(value = 1)
     private double price;
@@ -21,6 +27,7 @@ public class OrderDTO {
 
     public Order toEntity(){
         Order order = new Order();
+        order.setId(this.getId());
         order.setPrice(this.getPrice());
         order.setTotalAmount(this.getTotalAmount());
         return order;
@@ -28,6 +35,7 @@ public class OrderDTO {
 
     public static OrderDTO fromEntity(Order order){
         OrderDTO dto = new OrderDTO();
+        dto.setId(order.getId());
         dto.setPrice(order.getPrice());
         dto.setTotalAmount(order.getTotalAmount());
         return dto;
